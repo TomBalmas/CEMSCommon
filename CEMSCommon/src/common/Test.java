1e3a2fdee03ff46fcd061a3490bad9d081e01cdf
Tom Balmas <tombalmas12@gmail.com>
2021-05-23 23:06:06 +0300

    23:05 tests changed

--- CEMSCommon/src/common/Test.java
+++ CEMSCommon/src/common/Test.java
@@ -1,30 +1,62 @@
 package common;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Hashtable;
 
-public class Test {
+public class Test implements Serializable {
+	@Override
+	public String toString() {
+		return "Test [ID=" + ID + ", authorName=" + authorName + ", testName=" + testName + ", course=" + course
+				+ ", testDuration=" + testDuration + ", pointsPerQuestion=" + pointsPerQuestion + ", instructions="
+				+ instructions + ", teacherInstructions=" + teacherInstructions + ", field=" + field + ", testData="
+				+ testData + "]";
+	}
+
 	private int ID;
 	private String authorName;
 	private String testName;
 	private String course;
-	private Date testDuration;
-	private Hashtable<Question, Integer> pointsPerQuestion = new Hashtable();
-	private String examineeInstructions;
+	private String testDuration;
+	private String pointsPerQuestion;
+	private String instructions;
+	public int getID() {
+		return ID;
+	}
+
+	public String getTestName() {
+		return testName;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public String getField() {
+		return field;
+	}
+
 	private String teacherInstructions;
 	private String field;
 	private TestData testData;
 
-	public Test(int iD, String authorName, String testName, String course, Date testDuration,
-			Hashtable<Question, Integer> pointsPerQuestion, String examineeInstructions, String teacherInstructions,
-			String field) {
-		ID = iD;
+	public Test(int ID, String authorName, String testName, String course, String testDuration,
+			String pointsPerQuestion, String instructions, String teacherInstructions, String field) {
+		this.ID = ID;
 		this.authorName = authorName;
 		this.testName = testName;
 		this.course = course;
 		this.testDuration = testDuration;
 		this.pointsPerQuestion = pointsPerQuestion;
-		this.examineeInstructions = examineeInstructions;
+		this.instructions = instructions;
 		this.teacherInstructions = teacherInstructions;
 		this.field = field;
 	}
