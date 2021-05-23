1fd4957d22721562f2700340a291e7b61b85e67d
irisgrabois <irisgrabois@gmail.com>
2021-05-23 10:20:22 +0300

    working 23.5 common

--- CEMSCommon/src/common/Question.java
+++ CEMSCommon/src/common/Question.java
@@ -1,19 +1,30 @@
 package common;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Hashtable;
 
-public class Question {
+public class Question implements Serializable  {
 	private Integer ID;
 	private String author;
+	private String instructionsForTeacher;
+	private String instructionsForStudents;
 	private String questionText;
-	private  Hashtable<Question, Integer> rightAnswers = new Hashtable(); 
-	private  Hashtable<String, Question> answers = new Hashtable(); 
+	private int correctAnswer;
+	private  ArrayList<String> answers = new ArrayList<>();
 	private String field;
-	
-	public Question(Integer id, String author, String questionText,  String field) {
+	public String getAuthor() {
+		return author;
+	}
+	public Question(Integer id, String author,String instructionsForTeacher,String instructionsForStudents,String questionText,
+			int correctAnswer,String field,ArrayList<String> answers) {
 		this.ID = id;
 		this.author = author;
+		this.instructionsForTeacher=instructionsForTeacher;
+		this.instructionsForStudents=instructionsForStudents;
 		this.questionText = questionText;
+		this.correctAnswer=correctAnswer;
 		this.field = field;
+		this.answers=answers;
 	}
 } 
--- CEMSCommon/src/common/Teacher.java
+++ CEMSCommon/src/common/Teacher.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class Teacher extends User {
 	private ArrayList<String> fields = new ArrayList<>();
+	
+	public ArrayList<String> getFields() {
+		return fields;
+	}
+
 	public Teacher(int sSN, String name, String surName, String email, String userName, String password,ArrayList<String> fields) {
 		super(sSN, name, surName, userName, email, password);
 		this.fields=fields;
