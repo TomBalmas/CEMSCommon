0fd5bc916cf7b547ca036f6e405a2817bb36bf6a
TalAmar <f914gt29ra12@gmail.com>
2021-05-25 01:53:56 +0300

    00:00

--- CEMSCommon/src/common/Question.java
+++ CEMSCommon/src/common/Question.java
@@ -4,63 +4,71 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-public class Question implements Serializable  {
+public class Question implements Serializable {
 	private Integer ID;
-	
+
 	private String author;
 	private String instructionsForTeacher;
 	private String instructionsForStudents;
 	private String questionText;
 	private int correctAnswer;
-	private  ArrayList<String> answers = new ArrayList<>();
+	private ArrayList<String> answers = new ArrayList<>();
 	private String field;
+
 	public String getField() {
 		return field;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
-	
+
 	public Integer getID() {
 		return ID;
 	}
+
 	public void setID(Integer iD) {
 		ID = iD;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public void setField(String field) {
 		this.field = field;
 	}
-	public Question(Integer id, String author,String instructionsForTeacher,String instructionsForStudents,String questionText,
-			int correctAnswer,String field,ArrayList<String> answers) {
+
+	public Question(Integer id, String author, String instructionsForTeacher, String instructionsForStudents,
+			String questionText, int correctAnswer, String field, ArrayList<String> answers) {
 		this.ID = id;
 		this.author = author;
-		this.instructionsForTeacher=instructionsForTeacher;
-		this.instructionsForStudents=instructionsForStudents;
+		this.instructionsForTeacher = instructionsForTeacher;
+		this.instructionsForStudents = instructionsForStudents;
 		this.questionText = questionText;
-		this.correctAnswer=correctAnswer;
+		this.correctAnswer = correctAnswer;
 		this.field = field;
-		this.answers=answers;
+		this.answers = answers;
 	}
+
 	public String getInstructionsForTeacher() {
 		return instructionsForTeacher;
 	}
+
 	public String getInstructionsForStudents() {
 		return instructionsForStudents;
 	}
+
 	public String getQuestionText() {
 		return questionText;
 	}
+
 	public int getCorrectAnswer() {
 		return correctAnswer;
 	}
+
 	public ArrayList<String> getAnswers() {
 		return answers;
 	}
-	
-	
-	
-	
-} 
+
+}
