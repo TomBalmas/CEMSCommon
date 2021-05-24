bded336b8bc5f61b7b4da957804cd854a18737dd
TalAmar <f914gt29ra12@gmail.com>
2021-05-24 18:12:43 +0300

    18:00

--- CEMSCommon/src/common/ScheduledTest.java
+++ CEMSCommon/src/common/ScheduledTest.java
@@ -3,19 +3,52 @@ package common;
 import java.sql.Date;
 import java.sql.Time;
 
-public class ScheduledTest extends AbstractTest{
-	
-	private int id;
-	private String title;
-	private String author;
+public class ScheduledTest extends AbstractTest {
+
 	private Date date;
 	private Time startingTime;
 	private int duration;
-	private int belongsTo;
-	
-	public ScheduledTest(int ID, String authorName, String testName, String course) {
+	private int belongsTo; // id of the teacher that scheduled the test
+
+	public ScheduledTest(int ID, String authorName, String testName, String course, Date date, Time startingTime,
+			Integer duration, Integer belongsTo) {
 		super(ID, authorName, testName, course);
+		this.date = date;
+		this.startingTime = startingTime;
+		this.duration = duration;
+		this.belongsTo = belongsTo;
 	}
-	
-	
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Time getStartingTime() {
+		return startingTime;
+	}
+
+	public void setStartingTime(Time startingTime) {
+		this.startingTime = startingTime;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public int getBelongsTo() {
+		return belongsTo;
+	}
+
+	public void setBelongsTo(int belongsTo) {
+		this.belongsTo = belongsTo;
+	}
+
 }
