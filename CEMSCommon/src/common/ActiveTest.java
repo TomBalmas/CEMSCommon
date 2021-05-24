a7137fcedfd636f916012d34af840f09c9b42c77
Tal Amar <79013823+TalAmar@users.noreply.github.com>
2021-05-24 23:05:14 +0300

    Update ActiveTest.java

--- CEMSCommon/src/common/ActiveTest.java
+++ CEMSCommon/src/common/ActiveTest.java
@@ -2,10 +2,6 @@ package common;
 
 public class ActiveTest extends AbstractTest {
 	
-	private int ID;
-	private String testName;
-	private String authorName;
-	private String course;
 	private String startTimeTest;
 	private String endTimeTest;
 	private String field;
@@ -13,29 +9,9 @@ public class ActiveTest extends AbstractTest {
 	public ActiveTest(int ID, String authorName, String testName, String course, String field, String startTimeTest, String endTimeTest) {
 		
 		super(ID, authorName, testName, course);
-		this.ID = ID;
-		this.testName = testName;
-		this.authorName = authorName;
-		this.course = course;
 		this.field = field;
 		this.startTimeTest = startTimeTest;
 		this.endTimeTest = endTimeTest;
-	}
-
-	public int getID() {
-		return ID;
-	}
-
-	public String getTestName() {
-		return testName;
-	}
-
-	public String getAuthorName() {
-		return authorName;
-	}
-
-	public String getCourse() {
-		return course;
 	}
 
 	public String getStartTimeTest() {
@@ -52,7 +28,6 @@ public class ActiveTest extends AbstractTest {
 
 	@Override
 	public String toString() {
-		
 		return ID + ":" + testName + ":"+ authorName+course + ":" + startTimeTest + ":" + endTimeTest + ":"+ field;
 	}
 	
