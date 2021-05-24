0fcf444e2b52b76c8f28776b469519559ee3e482
Tom Balmas <tombalmas12@gmail.com>
2021-05-24 13:33:30 +0300

    changes for tal

--- CEMSCommon/src/common/Teacher.java
+++ CEMSCommon/src/common/Teacher.java
@@ -1,23 +1,28 @@
 package common;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Teacher extends User {
-	private ArrayList<String> fields = new ArrayList<>();
-	
-	public ArrayList<String> getFields() {
+	private List<String> fields;
+
+	public List<String> getFields() {
 		return fields;
 	}
 
-	public Teacher(int sSN, String name, String surName, String email, String userName, String password,ArrayList<String> fields) {
+	public Teacher(int sSN, String name, String surName, String email, String userName, String password,
+			String fields) {
 		super(sSN, name, surName, email, userName, password);
-		this.fields=fields;
+		String[] arr = fields.split(":");
+		this.fields = new ArrayList<>();
+		for (String field : arr)
+			this.fields.add(field);
+
 	}
-	
+
 	public String toString() {
-		return String.format("Teacher:%d,%s,%s,%s,%s,%s", getSSN(), getName(), getSurName(), getEmail(),
-				getUserName(), getPassword());
+		return String.format("Teacher:%d,%s,%s,%s,%s,%s", getSSN(), getName(), getSurName(), getEmail(), getUserName(),
+				getPassword());
 	}
 
 }
- 
