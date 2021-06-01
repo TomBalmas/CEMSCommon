1b336b729b12853f3248232b244602838b7cb9a9
TalAmar <f914gt29ra12@gmail.com>
2021-06-01 23:25:31 +0300

    23:20

--- CEMSCommon/src/common/ActiveTest.java
+++ CEMSCommon/src/common/ActiveTest.java
@@ -46,4 +46,38 @@ public class ActiveTest extends AbstractTest {
 		return getID() + "," + getAuthorName() + "," + getTitle() + "," + getCourse() + "," + field + ","
 				+ startingTime + "," + finishTime + "," + code;
 	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ActiveTest other = (ActiveTest) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (field == null) {
+			if (other.field != null)
+				return false;
+		} else if (!field.equals(other.field))
+			return false;
+		if (finishTime == null) {
+			if (other.finishTime != null)
+				return false;
+		} else if (!finishTime.equals(other.finishTime))
+			return false;
+		if (startingTime == null) {
+			if (other.startingTime != null)
+				return false;
+		} else if (!startingTime.equals(other.startingTime))
+			return false;
+		return true;
+	}
 }
